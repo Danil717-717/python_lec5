8ce34fa236160dd053477c3a177c5ac2bbaf500f
Danil <danila_monster@mail,ru>
2022-10-20 16:15:29 +0300

    bot second command

--- bot_command.py
+++ bot_command.py
@@ -1,19 +1,23 @@
 import datetime
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
+from spy import *
 
 
 async def hi_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
     await update.message.reply_text(f'HI  {update.effective_user.first_name}')
 
 async def help_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
     await update.message.reply_text(f'/hi\n/time\n/help')  
 
 async def time_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
 
 async def sum_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
     msg = update.message.text
     print(msg)
     items = msg.split() # /sum 123 345
