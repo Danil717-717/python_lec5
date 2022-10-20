6fd1733d0c61e413baf93219993338f7ae331baa
Danil <danila_monster@mail,ru>
2022-10-20 14:25:47 +0300

    add bot

--- main.py
+++ main.py
@@ -1,58 +1,13 @@
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-list = [1, 2, 3, 2, 7]
-plt.plot(list)
-
-plt.show()
-
-#plt.style.use('_mpl-gallery')
-
-#$ make data
-#np.random.seed(3)
-#x =  0.5 + np.arange(8)
-#y = np.random.uniform(2,7 , len(x))
-
-## plot
-#fig, ax = plt.subplots()
-
-#ax.stem(x, y)
-
-#ax.set(xlim=(0,8 ), xticks=np.arange(1,8 ),
-#       ylim=(0,8 ), yticks=np.arange(1,8 ))
-
-#plt.show()
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
+async def hello (update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello  {update.effective_user.first_name}')
 
 
+app = ApplicationBuilder().token("5614398079:AAHG5xeB3Tza3l1P0Y7vOVDPIKpK1huPglo").build()
 
-#import emoji
-
-#print(emoji.emojize('Python is :thumbs_up:'))
-#print(emoji.emojize('Python is :thumbsup:', language='alias'))
-#print(emoji.demojize('Python is 👍'))
-#print(emoji.emojize("Python is fun :red_heart:"))
-#print(emoji.emojize("Python is fun :red_heart:", variant="emoji_type"))
-#print(emoji.is_emoji("👍"))
-
-
-
-
-#from progress.bar import Bar
-#import time
-
-#bar = Bar('Processing', max=20)
-#for i in range(20):
-#    time.sleep(1)
-#    bar.next()
-#bar.finish()
-
-
-
-
-#from isOdd import isOdd
-
-#print(isOdd(0)) 
-#print(isOdd(5))
+app.add_handler(CommandHandler("hello", hello))
+print("Server start")
+app.run_polling()
