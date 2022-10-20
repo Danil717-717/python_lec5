f60dc233fe9107e4bf75c53c5b3e0ed63a67c8d5
Danil <danila_monster@mail,ru>
2022-10-20 15:32:03 +0300

    bot first command

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-
-async def hello (update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello  {update.effective_user.first_name}')
-
+from bot_command import*
 
 app = ApplicationBuilder().token("5614398079:AAHG5xeB3Tza3l1P0Y7vOVDPIKpK1huPglo").build()
 
-app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+
 print("Server start")
 app.run_polling()
